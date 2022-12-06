b46a779eea45d9072c1e377fdc150e73913e858f
thomasvsl <91764036+thomasvsl@users.noreply.github.com>
2022-12-06 17:02:20 +0100

    script push upd

--- Global Consult/Q1/push_script.py
+++ Global Consult/Q1/push_script.py
@@ -4,7 +4,14 @@ import subprocess
 rootdir = '.'
 for file in os.listdir(rootdir):
     d = os.path.join(rootdir, file)
-    if os.path.isdir(d) and d != ".\\utils":
+    if os.path.isdir(d) and d != ".\\utils" and d != ".\\0 Complet Q1":
+        print(f"----------PUSHING {d} DIR------------")
+        os.chdir(d)
+        os.system("python test.py -c -f")
+        os.chdir("..")
+for file in os.listdir(rootdir):
+    d = os.path.join(rootdir, file)
+    if os.path.isdir(d) and d == ".\\0 Complet Q1":
         print(f"----------PUSHING {d} DIR------------")
         os.chdir(d)
         os.system("python test.py -c -f")
